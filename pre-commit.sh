7734b204a1a774546cda97afc10d21cc9808b17e
corinacioloca <corina.cioloca@gmail.com>
2020-06-26 11:19:22 +0300

    [EP-1226] Front End - Widget - Download widget data

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm install --package-lock-only --ignore-scripts && npm run build:dist && git add dist package-lock.json src/core/_icon-font.scss
+npm install --package-lock-only --ignore-scripts && git add dist package-lock.json src/core/_icon-font.scss
